fb762f27494435d89a023c149f9119b82bd193a8
Alicia Rojo <aliciarojovera@hotmail.com>
2020-11-03 13:07:54 +0100

    todo

---
+++ enemy2.js
@@ -0,0 +1,11 @@
+class Enemy2 extends Enemys{
+    constructor(ctx, speed, damage, score, enemySizew, enemySizeh, enemyImage ) {
+        super(ctx, speed, damage, score, enemySizew, enemySizeh, enemyImage)
+      
+    }
+
+  
+        
+    }
+    
+
